b1b126162338ebc33dee2deeccbcbeff44097afd
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-06-22 23:50:08 +0530

    Create Variables and Operations.js

---
+++ Variables and Operations/Variables and Operations.js
@@ -0,0 +1,7 @@
+var name = "Chanith";   // using 'var' (older way)
+let age = 23;           // using 'let' (modern)
+const country = "Sri Lanka";  // constant value
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
