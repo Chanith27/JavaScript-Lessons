110c82cb4d0e2221cc1d5a61713cfb31f81105f0
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-06-22 23:50:43 +0530

    Update Variables and Operations.js

--- Variables and Operations/Variables and Operations.js
+++ Variables and Operations/Variables and Operations.js
@@ -5,3 +5,14 @@ const country = "Sri Lanka";  // constant value
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Country:", country);
+
+ // Basic Data Types
+    let isStudent = true;
+    let height = 5.9;
+    let score = null;
+    let undefinedVar;
+
+    console.log("Is Student?", isStudent);
+    console.log("Height:", height);
+    console.log("Score:", score);
+    console.log("Undefined Variable:", undefinedVar);
