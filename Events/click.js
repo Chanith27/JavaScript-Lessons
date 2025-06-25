1a6964ca652d297d201886774b7627198a74c13a
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-06-25 23:46:39 +0530

    Create click.js

---
+++ Events/click.js
@@ -0,0 +1,9 @@
+<button id="myBtn">Click Me</button>
+
+<script>
+  const btn = document.getElementById("myBtn");
+
+  btn.addEventListener("click", function () {
+    alert("You clicked the button!");
+  });
+</script>
